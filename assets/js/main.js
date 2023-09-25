29e86e9267743f2a8b9a7e7a75b55450249c0ac2
Raj Sudharshan <sudharshann05@gmail.com>
2023-09-25 00:50:23 -0700

    fix icon not rendering ios issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,22 +167,22 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true //Animations repeat
-})
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400,
+//     // reset: true //Animations repeat
+// })
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
-sr.reveal(`.home__name, .home__info,
-            .about__container .section__title-1, .about__info,
-            .contact__social, .contact__data`, {origin: 'left'})
-sr.reveal(`.skills__card, .projects__card`, {interval: 100})
-sr.reveal('.section__title-2', {origin: 'top'});
-sr.reveal('.section__title-1', {origin: 'top'}); 
-sr.reveal('.exp__toggle', {origin: 'left', interval: 100}); 
-// sr.reveal('.details', {origin: 'right'});
+// sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
+// sr.reveal(`.home__name, .home__info,
+//             .about__container .section__title-1, .about__info,
+//             .contact__social, .contact__data`, {origin: 'left'})
+// sr.reveal(`.skills__card, .projects__card`, {interval: 100})
+// sr.reveal('.section__title-2', {origin: 'top'});
+// sr.reveal('.section__title-1', {origin: 'top'}); 
+// sr.reveal('.exp__toggle', {origin: 'left', interval: 100}); 
+// // sr.reveal('.details', {origin: 'right'});
 
 
